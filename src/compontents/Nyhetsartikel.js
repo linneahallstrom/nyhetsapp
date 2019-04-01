8428c024705c7e6490bf9383e1b6e58d14258869
Linnea Hallström <linneahallstrom@outlook.com>
2019-04-01 11:04:43 +0200

    lagt till egna komponenter

---
+++ src/compontents/Nyhetsartikel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nyhetsartikel (){
+    return (<article>
+        <img src="https://source.unsplash.com/random/400x350/?dog" />
+        <h2>Testnyhet</h2>
+        <p>Beskrivning av testnyheter</p>
+        <a>Läs mer..</a>
+      </article>);
+}
+
+export default Nyhetsartikel; 
